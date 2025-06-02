7d54d3a6661cb8261e5977f02b7cdd1e97d021e9
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2025-06-02 07:48:05 +0300

    Added Spring configuration

---
+++ src/main/java/com/example/electricity_bot/ElectricityBotApplication.java
@@ -0,0 +1,14 @@
+package com.example.electricity_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityBotApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/com/example/electricity_bot/ElectricityBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electricity_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
