b85ca3d67084e5e77fad2d11084c6530a70c209c
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2025-07-18 14:07:24 +0300

    Added DeviceHistoryResponse dto

--- src/main/java/com/example/electricity_bot/dto/DeviceHistoryResponse.java
+++ src/main/java/com/example/electricity_bot/dto/DeviceHistoryResponse.java
@@ -1,4 +1,21 @@
 package com.example.electricity_bot.dto;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
-public record DeviceHistoryResponse(String status, LocalDateTime timestamp) {}
+public class DeviceHistoryResponse {
+    private final String status;
+    private final String timestamp;
+
+    public DeviceHistoryResponse(String status, String timestamp) {
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+}
