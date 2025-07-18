68e31ffd4986e9f8911dfc6c326b29f5b6d05206
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2025-07-18 13:56:10 +0300

    Added DeviceStatusHistoryRepository and DeviceStatusRepository

--- src/main/java/com/example/electricity_bot/repositories/DeviceStatusHistoryRepository.java
+++ src/main/java/com/example/electricity_bot/repositories/DeviceStatusHistoryRepository.java
@@ -2,6 +2,7 @@ package com.example.electricity_bot.repositories;
 import com.example.electricity_bot.model.DeviceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface DeviceStatusHistoryRepository extends JpaRepository<DeviceHistory, Long> {
--- src/main/java/com/example/electricity_bot/repositories/DeviceStatusRepository.java
+++ src/main/java/com/example/electricity_bot/repositories/DeviceStatusRepository.java
@@ -1,14 +1,16 @@
 package com.example.electricity_bot.repositories;
+import com.example.electricity_bot.model.Device;
 import com.example.electricity_bot.model.DeviceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DeviceStatusRepository extends JpaRepository<DeviceStatus, String> {
     List<DeviceStatus> findByTimestampBeforeAndStatusNot(LocalDateTime timestamp, String status);
-
+    Optional<DeviceStatus> findByDevice(Device device);
 
 }
